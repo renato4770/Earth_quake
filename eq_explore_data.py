ee8c766bc2578780d3ba8e1e58a8e7a74f1c18fb
renato4770 <118706576+renato4770@users.noreply.github.com>
2022-12-21 22:49:16 +0000

    Added Refactoring and Title with dynamic data
    In this feature I added dynamic data as well as refactor the code and automated titles when a json file is loaded from 
    earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -9,27 +9,28 @@ filename = '/home/renato/Documents/data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
+text_file = all_eq_data['metadata']
 all_eq_data = all_eq_data['features']
 
-mags, lons, lats = [],[],[]
+mags, lons, lats, hover_text = [],[],[],[]
 for eq_dict in all_eq_data:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_text.append(eq_dict['properties']['title'])
 
+
+show_textfile = text_file['title']
 # Map the earthquakes
-
 data = [{
     'type':'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_text,
     'marker':{
         'size': [3*mag for mag in mags],
         'color': mags,
-        'colorscale':'Viridis',
+        'colorscale':'YlGnBu',
         'reversescale':True,
         'colorbar':{
             'title':'Magnitude',
@@ -37,7 +38,8 @@ data = [{
     }
 
 }]
-my_layout = Layout(title='Global Earthquakes')
+
+my_layout = Layout(title=show_textfile)
 
 fig = {'data':data, 'layout':my_layout}
 offline.plot(fig,filename='global_earthqueakes.html')
